ee394ee85dcda314487d407b0234ca0928db91ac
Mo Ansari <paytakhtdota@gmail.com>
2023-09-02 21:24:50 -0700

    section2 added - js for cards added

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+function change2UnderHtml() {
+    document.getElementById("html-jpg").src = ("./img/under-html.jpg");
+}
+
+function changeHtml() {
+    document.getElementById("html-jpg").src = ("./img/html.jpg");
+}
+
+function change2UnderCss() {
+    document.getElementById("css-jpg").src = ("./img/under-css.jpg");
+}
+
+function changeCss() {
+    document.getElementById("css-jpg").src = ("./img/css.jpg");
+}
+
+function change2UnderJs() {
+    document.getElementById("js-jpg").src = ("./img/under-js.jpg");
+}
+
+function changeJs() {
+    document.getElementById("js-jpg").src = ("./img/js.jpg");
+}
